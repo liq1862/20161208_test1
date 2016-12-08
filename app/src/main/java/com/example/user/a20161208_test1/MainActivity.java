b961039cc15d065b87d77434e69a1a47eadfbeee
lili <liq1862@gmail.com>
2016-12-08 10:25:34 +0800

    Button for list and add

--- app/src/main/java/com/example/user/a20161208_test1/MainActivity.java
+++ app/src/main/java/com/example/user/a20161208_test1/MainActivity.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,34 +15,74 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+    final String DB_NAME = "student.sqlite";
+    String DB_FILE;
+    EditText ed1, ed2, ed3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DB_FILE = getFilesDir() + File.separator + DB_NAME;
+        ed1 = (EditText) findViewById(R.id.editText);
+        ed2 = (EditText) findViewById(R.id.editText2);
+        ed3 = (EditText) findViewById(R.id.editText3);
 
-        try {
-            String DB_FILE = getFilesDir() + File.separator + "student.sqlite";
-            InputStream is = MainActivity.this.getAssets().open("student.sqlite");
-            OutputStream os = new FileOutputStream(DB_FILE);
+        copyDataBaseFile();
+    }
 
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = is.read(buffer)) > 0)
-            {
-                os.write(buffer, 0, length);
+    public void clickAdd(View v)
+    {
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(DB_FILE, null);
+        String SName = ed1.getText().toString();
+        String tel = ed2.getText().toString();
+        String addr = ed3.getText().toString();
+        String sql = "insert into phone (SName, tel, addr) values ('" + SName + "','" + tel + "','" + addr + "')";
+        db.execSQL(sql);
+        db.close();
+    }
+
+    public void clickList(View v)
+    {
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(DB_FILE, null);
+        Cursor c = db.rawQuery("Select * from phone", null);
+        if (c.moveToFirst())
+        {
+            Log.d("DB", c.getString(1));
+        }
+
+        while(c.moveToNext())
+        {
+            Log.d("DB", c.getString(1));
+        }
+        c.close();
+        db.close();
+    }
+
+    private void copyDataBaseFile()
+    {
+        File f = new File(DB_FILE);
+
+        if (!f.exists())
+        {
+            InputStream is = null;
+            try {
+                is = MainActivity.this.getAssets().open(DB_NAME);
+                OutputStream os = new FileOutputStream(f);
+
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = is.read(buffer)) > 0)
+                {
+                    os.write(buffer, 0, length);
+                }
+                os.flush();
+                os.close();
+                is.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            os.flush();
-            os.close();
-            is.close();
 
-            SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(DB_FILE, null);
-            Cursor c = db.rawQuery("Select * from phone", null);
-            c.moveToFirst();
-            String str = c.getString(1);
-            Log.d("DB", str);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
